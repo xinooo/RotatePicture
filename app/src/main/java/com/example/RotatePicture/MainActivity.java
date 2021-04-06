eb0c35295e81d2c94d94f7a63f11adaecd641fa8
cody.chen <cody.chen@itnsl.net>
2021-04-06 09:24:28 +0800

    ddd

--- app/src/main/java/com/example/RotatePicture/MainActivity.java
+++ app/src/main/java/com/example/RotatePicture/MainActivity.java
@@ -4,12 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.graphics.Matrix;
-import android.graphics.PixelFormat;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.DisplayMetrics;
@@ -19,22 +14,36 @@ import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView img;
+    private ImageView img, img2;
     private SeekBar seekBar;
-    private int agree = 0;
+    private int originalAngle = -150;   //初始角度
+    private int newAngle;               //目前角度
+    private int mProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         img = (ImageView)findViewById(R.id.pic);
+        img2 = (ImageView)findViewById(R.id.pic2);
         seekBar = (SeekBar) findViewById(R.id.seekBar);
+        reset();
+    }
+
+    private void reset(){
+        img2.setVisibility(View.GONE);
+        img.setImageBitmap(rotateBitmap(BitmapFactory.decodeResource(getResources(), R.mipmap.pic2, null), originalAngle));
+        seekBar.setThumb(getResources().getDrawable(R.drawable.seekbar_thumb_start));
+        seekBar.setSplitTrack(false);
+        seekBar.setProgress(0);
+        seekBar.setThumbOffset(0);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
-                img.setImageDrawable(rotateBitmap(BitmapFactory.decodeResource(getResources(), R.mipmap.pic2, null), progress));
-//                img.animate().rotation(progress);
-                agree = progress;
+                mProgress = progress;
+                newAngle = progress+originalAngle;
+                newAngle = newAngle <0? 360+ newAngle : newAngle;  //角度轉正
+                img.setImageBitmap(rotateBitmap(BitmapFactory.decodeResource(getResources(), R.mipmap.pic2, null), newAngle));
             }
 
             @Override
@@ -44,19 +53,26 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(final SeekBar seekBar) {
-                seekBar.setThumb(getResources().getDrawable(R.drawable.seekbar_thumb_failed));
-                seekBar.setThumbOffset(dp2px(30)*agree/360);
-                seekBar.setSplitTrack(false);
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        seekBar.setThumb(getResources().getDrawable(R.drawable.seekbar_thumb));
-                        seekBar.setSplitTrack(false);
-                        seekBar.setProgress(0);
-                        seekBar.setThumbOffset(0);
-                    }
-                },1000);
-
+                if (newAngle > -15 && newAngle < 15){
+                    img2.setImageResource(getResources().getIdentifier("a_src_success", "mipmap", getPackageName()));
+                    img2.setVisibility(View.VISIBLE);
+                    seekBar.setThumb(getResources().getDrawable(R.drawable.seekbar_thumb_success));
+                    seekBar.setThumbOffset(dp2px(30)*mProgress/360);
+                    seekBar.setSplitTrack(false);
+                    seekBar.setEnabled(false);
+                }else {
+                    img2.setImageResource(getResources().getIdentifier("a_src_error", "mipmap", getPackageName()));
+                    img2.setVisibility(View.VISIBLE);
+                    seekBar.setThumb(getResources().getDrawable(R.drawable.seekbar_thumb_failed));
+                    seekBar.setThumbOffset(dp2px(30)*mProgress/360);
+                    seekBar.setSplitTrack(false);
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            reset();
+                        }
+                    },1000);
+                }
             }
         });
     }
@@ -67,9 +83,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public Drawable rotateBitmap(Bitmap bitmap, int degrees) {
+    public Bitmap rotateBitmap(Bitmap bitmap, int degrees) {
         if (degrees == 0 || null == bitmap) {
-            return new BitmapDrawable(this.getResources(), bitmap);
+            return bitmap;
         }
         Matrix matrix = new Matrix();
         matrix.setRotate(degrees, bitmap.getWidth() / 2, bitmap.getHeight() / 2);
@@ -77,6 +93,6 @@ public class MainActivity extends AppCompatActivity {
         if (null != bitmap) {
             bitmap.recycle();
         }
-        return new BitmapDrawable(this.getResources(), bmp);
+        return bmp;
     }
 }
